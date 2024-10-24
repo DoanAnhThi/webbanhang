a533601dc47953df3bbb2244a0e8af9d0054acef
AnhThi <bincalam28499@gmail.com>
2024-10-24 14:48:50 +0700

    Lay thong tin san pham tu co so du lieu qua

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
-    context={}
-    return render(request,'app/home.html')
+    products = Product.objects.all()
+    context={'products':products}
+    return render(request,'app/home.html', context)
 def cart(request):
     context={}
     return render(request,'app/cart.html', context)
