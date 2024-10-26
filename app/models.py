4d0e6490da45b085729be59dac586aaaca7325b6
AnhThi <bincalam28499@gmail.com>
2024-10-26 09:49:28 +0700

    thay thong tin tu co so du lieu sang trang cart nhung bi hu file view

--- app/models.py
+++ app/models.py
@@ -18,8 +18,16 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    @property
+    def ImageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ""
+        return url
         
-class Oder(models.Model):
+        
+class Order(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
     date_order = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False,null=True,blank=False)
@@ -28,15 +36,15 @@ class Oder(models.Model):
     def __str__(self):
         return str(self.id)
     
-class OderItem(models.Model):
+class OrderItem(models.Model):
     product = models.ForeignKey(Product,on_delete=models.SET_NULL,blank=True,null=True)
-    oder = models.ForeignKey(Oder,on_delete=models.SET_NULL,blank=True,null=True)
+    order = models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True,null=True)
     quantity = models.IntegerField(default=0,null=True,blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
-    oder = models.ForeignKey(Oder,on_delete=models.SET_NULL,blank=True,null=True)
+    order = models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True,null=True)
     address = models.CharField(max_length=200,null=True)
     city = models.CharField(max_length=200,null=True)
     state = models.CharField(max_length=200,null=True)
--- app/views.py
+++ app/views.py
@@ -8,8 +8,15 @@ def home(request):
     context={'products':products}
     return render(request,'app/home.html', context)
 def cart(request):
-    context={}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, completed = False)
+        items = order.orderitem_set.all()
+    else:
+        items = []        
+        context={'items':items, 'order':order}
     return render(request,'app/cart.html', context)
 def checkout(request):
     context={}
     return render(request,'app/checkout.html', context)
+    
