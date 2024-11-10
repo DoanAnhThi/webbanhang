e79e3276db82f3bc2c3a095fe658f159151266c3
AnhThiPro <bincalam28499@gmail.com>
2024-11-10 14:48:42 +0700

    tao trang register va trang login, tich hop vao base

--- app/views.py
+++ app/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from .models import *
 import json
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def register(request):
+    form = CreateUserForm()
+    context = {'form':form}
+    if request.method =="POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request,'app/register.html', context)
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method =="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else: messages.info(request,'user or password not correct!')
+    context = {}
+    return render(request,'app/login.html', context)
+
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
+
+
+
 def home(request):
     if request.user.is_authenticated:
         customer = request.user.customer
