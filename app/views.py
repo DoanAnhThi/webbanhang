27857c54d2ceb379927a07ed837ac87469671dd7
AnhThiPro <bincalam28499@gmail.com>
2024-11-11 15:18:22 +0700

    Them tinh nang search. An hien khu vuc dang ky hop ly, nhung khi logout thi web ko hoat dong

--- app/views.py
+++ app/views.py
@@ -7,6 +7,23 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
+def search(request):
+    if request.method =="POST":
+        searched = request.POST["searched"]
+        keys = Product.objects.filter(name__contains = searched)
+    if request.user.is_authenticated:
+        customer = request.user
+        order, created = Order.objects.get_or_create( customer = customer, complete = False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'order.get_cart_items':0,'order.get_cart_total':0}
+        cartItems = order['get_cart_items']
+    products = Product.objects.all()
+    
+    return render(request,'app/search.html',{"searched":searched,"keys":keys, 'products':products,'cartItems':cartItems})
+
 def register(request):
     form = CreateUserForm()
     context = {'form':form}
@@ -14,8 +31,10 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+        return redirect('login')
     context = {'form':form}
     return render(request,'app/register.html', context)
+
 def loginPage(request):
     if request.user.is_authenticated:
         return redirect('home')
@@ -30,55 +49,72 @@ def loginPage(request):
     context = {}
     return render(request,'app/login.html', context)
 
+
 def logoutPage(request):
     logout(request)
     return redirect('login')
 
 
-
 def home(request):
     if request.user.is_authenticated:
-        customer = request.user.customer
+        customer = request.user
         order, created = Order.objects.get_or_create( customer = customer, complete = False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
+        user_not_login = "hidden"
+        user_login = "show"
     else:
         items = []
         order = {'order.get_cart_items':0,'order.get_cart_total':0}
         cartItems = order['get_cart_items']
+        user_login = "hidden"
+        user_not_login = "show"
     products = Product.objects.all()
-    context={'products':products,'cartItems':cartItems}
+    context={'products':products,'cartItems':cartItems,'user_login':user_login,'user_not_login':user_not_login}
     return render(request,'app/home.html', context)
+
+
 def cart(request):
     if request.user.is_authenticated:
-        customer = request.user.customer
+        customer = request.user
         order, created = Order.objects.get_or_create( customer = customer, complete = False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
+        user_not_login = "hidden"
+        user_login = "show"
     else:
         items = []
         order = {'order.get_cart_items':0,'order.get_cart_total':0}
         cartItems = order['get_cart_items']
-    context={'items': items, 'order':order,'cartItems':cartItems}
+        user_login = "hidden"
+        user_not_login = "show"
+    context={'items': items, 'order':order,'cartItems':cartItems,'user_login':user_login,'user_not_login':user_not_login}
     return render(request,'app/cart.html', context)
+
+
 def checkout(request):
     if request.user.is_authenticated:
-        customer = request.user.customer
+        customer = request.user
         order, created = Order.objects.get_or_create( customer = customer, complete = False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
+        user_not_login = "hidden"
+        user_login = "show"
     else:
         items = []
         order = {'order.get_cart_items':0,'order.get_cart_total':0}
         cartItems = order['get_cart_items']
-    context={'items': items, 'order':order,'cartItems':cartItems}
+        user_login = "hidden"
+        user_not_login = "show"
+    context={'items': items, 'order':order,'cartItems':cartItems,'user_login':user_login,'user_not_login':user_not_login}
     return render(request,'app/checkout.html', context)
+
 
 def updateItem(request):
     data = json.loads(request.body)
     productID = data['productID']
     action = data['action']
-    customer = request.user.customer
+    customer = request.user
     product = Product.objects.get(id = productID)
     order, created = Order.objects.get_or_create( customer = customer, complete = False)
     orderItem, created = OrderItem.objects.get_or_create( order = order, product = product)
